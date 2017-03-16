af01bbee0f6c6d47e166a687897092feb3d835ec
Pete Whitehead <peter.whitehead@intapp.com>
2017-03-16 13:40:38 +0000

    Create temphumidity.py

---
+++ sensors/temphumidity.py
@@ -0,0 +1,18 @@
+import RPi.GPIO as GPIO
+import dht11
+import sys
+
+pin = 7
+
+GPIO.setmode(GPIO.BOARD)
+
+instance = dht11.DHT11(pin)
+
+result = instance.read()
+if result.is_valid():
+  #  print("Last valid input: " + str(datetime.datetime.now()))
+  #  print("Temperature: %d C" % result.temperature)
+ #   print("Humidity: %d %%" % result.humidity)
+  #  print result.temperature
+
+print 13
