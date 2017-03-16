3dad7328d64b2193a0cb21df2f3b9979ce399854
Pete Whitehead <peter.whitehead@intapp.com>
2017-03-16 13:43:29 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,3 +5,6 @@ sudo python install/install_system.py
 
 # Python requirements
 sudo pip install -r requirements.txt
+
+# Temp humidity sensor code
+sudo curl https://raw.githubusercontent.com/szazo/DHT11_Python/master/dht11.py > dht11.py
