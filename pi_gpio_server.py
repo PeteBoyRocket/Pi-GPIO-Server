1443a2d536b4966d933f864a168fad62bab4be0e
PeteBoyRocket <peteboyrocket@hotmail.com>
2017-03-17 18:23:53 +0000

    comment pout

--- pi_gpio_server.py
+++ pi_gpio_server.py
@@ -1,5 +1,5 @@
 from pi_gpio import app, socketio
-from light.lightManager import lightManager
+#from light.lightManager import lightManager
 
 if __name__ == '__main__':
     socketio.run(app, host="0.0.0.0")
