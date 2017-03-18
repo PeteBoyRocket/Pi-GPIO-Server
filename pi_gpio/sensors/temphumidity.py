7a1b8db0252be8a230353d1240eba601f5c25219
PeteBoyRocket <peteboyrocket@hotmail.com>
2017-03-18 07:52:58 +0000

    rfgfg

--- pi_gpio/sensors/temphumidity.py
+++ pi_gpio/sensors/temphumidity.py
@@ -1,4 +1,5 @@
 #import RPi.GPIO as GPIO
+import time
 import dht11
 #import sys
 
@@ -11,12 +12,21 @@ DHT11 = dht11.DHT11(pin)
 class TempHumiditySensor():
 
     def getTempAndHumidity(self):
-        result = DHT11.read()
-        if result.is_valid():
-      #  print("Last valid input: " + str(datetime.datetime.now()))
-      #  print("Temperature: %d C" % result.temperature)
-    #   print("Humidity: %d %%" % result.humidity)
-      #  print result.temperature
-            return result.temperature, result.humidity
 
-        return "NA", "NA"
+        for i in range(15):
+            result = DHT11.read()
+            if result.is_valid():
+                return result.temperature, result.humidity
+            time.sleep(1)
+        return (None, None)
+
+
+    #     result = DHT11.read()
+    #     if result.is_valid():
+    #   #  print("Last valid input: " + str(datetime.datetime.now()))
+    #   #  print("Temperature: %d C" % result.temperature)
+    # #   print("Humidity: %d %%" % result.humidity)
+    #   #  print result.temperature
+    #         return result.temperature, result.humidity
+
+    #     return "NA", "NA"
